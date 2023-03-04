8f82bc115b22239ebd7e797a2a75a038f57baf4c
MaherHez <96958005+MaherHez@users.noreply.github.com>
2023-03-04 15:46:13 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Navbar from './Navbar';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
+
+const MyButton = styled.button`
+  padding: 4em;
+  background-color: papayawhip;
+`;
+
+function App() {
+
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/about">
+            <p>About</p>
+          </Route>
+          <Route path="/portfolio">
+            <p>Portfolio</p>
+          </Route>
+          <Route path="/">
+            <p>Home</p>
+          </Route>
+        </Switch>
+        <h1>Hello</h1>
+
+      </div>
+    </Router>
+  );
+}
+
+
+export default App;
